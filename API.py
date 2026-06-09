32c32e84690f1eae02ad4ca426efeb76c82d2748
Will <22300044@bucks.ac.uk>
2026-06-09 22:10:27 +0100

    better search
    I dont know what to do

--- API.py
+++ API.py
@@ -2,14 +2,18 @@ import requests
 import yaml
 from getpass import getpass
 import os
+import json
+
+from DEC import Search
 
 CONF_FILE = "config.yml"
 
 class GovApi :
     URI = "https://api.get-energy-performance-data.communities.gov.uk"
 
-    def __init__(self):
+    def __init__(self) :
         self.secret = None
+        self.get_secret()
 
     @property
     def config(self) :
@@ -23,17 +27,35 @@ class GovApi :
         with open(CONF_FILE,'w') as file:
             yaml.dump({'Auth-Token':value},file)
 
-    def get_secret(self):
+    def get_secret(self) :
         try :
             self.secret = self.config['Auth-Token']
         except KeyError, TypeError :
-            self.secret = getpass("Please enter your API Secret: ")
+            self.secret = getpass("Please enter your Bearer Token: ")
             self.config = self.secret
+    
+    def get_data(self,path,**kwargs) :
+        data = requests.get(GovApi.URI + path, headers= {
+            "Authorization": f"Bearer {self.secret}",
+            "Accept": "application/json"
+        }, params=kwargs)
+        print(data.url)
+
+        
+        if data.status_code != 200 :
+            print(data.status_code, data.json()["data"]["error"])
+        else :
+            print(json.dumps(data.json(), indent=2))
 
 if __name__ == "__main__":
     api = GovApi()
-    
-    api.get_secret()
+    # Working data centre address
+    # address = "13 Liverpool Road"
 
-    print(api.secret)
+    newParams = Search()
+    newParams.address = "13 Liverpool Road"
+    newParams.set_date(2017,2026)
+
+    api.get_data(Search.path,**newParams.params)
+
         
--- DEC.py
+++ DEC.py
@@ -1,24 +1,67 @@
-from datetime import date
+from datetime import date, timedelta
 
-date.fromisoformat()
-date.isoformat()
+class Search :
+    path = "/api/display/search"
 
-
-class DEC :
     @property
     def date_today(self) -> date :
         return date.today()
     
     @property
-    def year_begining(self) -> date :
-        return date(self.date_today.year,1,1)
+    def constituency(self) -> tuple :
+        return self.__region
+    @property
+    def postcode(self) -> tuple :
+        return self.__postcode
+    @property
+    def address(self) -> tuple :
+        return self.__address
+    
+    @constituency.setter
+    def constituency(self,value) :
+        self.__region = ("constituency",[value])
+    @postcode.setter
+    def postcode(self,value) :
+        self.__postcode = ("postcode",value)
+    @address.setter
+    def address(self,value) :
+        self.__address = ("address",value)
+    
+    @property
+    def params(self) -> dict :
+        result : dict = {}
+
+        if self.constituency : result |= dict([self.constituency])
+        if self.postcode : result |= dict([self.postcode])
+        if self.address : result |= dict([self.address])
+
+        if not self.start_date : return result
+        result["date_start"] = self.start_date.isoformat()
+        result["date_end"] = self.end_date.isoformat()
+        return result
 
     def __init__(self,):
-        pass
+        self.start_date : None|date = None
+        self.__region = ()
+        self.__postcode = ()
+        self.__address = ()
         
     def set_date(self,start_year,end_year) :
+
+        if end_year == self.date_today.year :
+            end = self.date_today - timedelta(days=1)
+        else : end = date(end_year,12,31)
+
         self.start_date : date = date(start_year,1,1)
-        self.end_date : date = date(end_year,12,31)
+        self.end_date : date = end
 
     def get_data(self) :
         pass
+
+if __name__ == "__main__":
+    
+    apiParams = Search()
+    print(apiParams.params)
+    apiParams.set_date(2026,2026)
+    print(apiParams.params)
+    # {'date_start': '2026-01-01', 'date_end': '2026-06-08'}
